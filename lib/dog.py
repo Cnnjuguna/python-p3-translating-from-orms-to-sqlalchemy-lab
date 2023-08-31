8d5eef0e8d7aac97efaebb4bb72434f5b241489f
Cnnjuguna <123736824+Cnnjuguna@users.noreply.github.com>
2023-08-31 06:35:06 +0300

    completed assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,36 @@
 from models import Dog
 
-def create_table(base):
-    pass
+
+def create_table(Base, engine):
+    # Create the database tables
+    Base.metadata.create_all(engine)
+
 
 def save(session, dog):
-    pass
+    session.add(dog)
+    session.commit()
+
 
 def get_all(session):
-    pass
+    return session.query(Dog).all()
+
 
 def find_by_name(session, name):
-    pass
+    return session.query(Dog).filter_by(name=name).first()
+
 
 def find_by_id(session, id):
-    pass
+    return session.query(Dog).filter_by(id=id).first()
+
 
 def find_by_name_and_breed(session, name, breed):
+    return session.query(Dog).filter_by(name=name, breed=breed).first()
     pass
 
-def update_breed(session, dog, breed):
+
+def update_breed(session, dog, new_breed):
+    dog.breed = new_breed
+    # return session.query(Dog).filter_by(breed=breed).first()
+    session.commit()
+
     pass
